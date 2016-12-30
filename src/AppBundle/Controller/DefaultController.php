6e287682a4890b20eb31068ab30598bdb47ece72
swiewior <sbswwr+bitbucket@gmail.com>
2016-12-30 20:21:20 +0100

    Theme: startbootstrap-sb-admin-2 - Pasek górny - Menu - Logowanie

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,7 +2,6 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -15,6 +14,17 @@ class DefaultController extends Controller
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/blank", name="blank")
+     */
+    public function blankAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/blank.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
     }
--- src/AppBundle/Menu/Builder.php
+++ src/AppBundle/Menu/Builder.php
@@ -12,13 +12,72 @@ class Builder implements ContainerAwareInterface
 
     public function mainMenu(FactoryInterface $factory, array $options)
     {
-        $menu = $factory->createItem('root');
+        $menu = $factory->createItem('root', array(
+            'childrenAttributes'    => array(
+                'class'             => 'nav',
+                'id'                => 'side-menu',
+            ),
+        ));
+//        $menu->setChildrenAttribute('class',null);
 
-        $menu->addChild('Home', array('route' => 'index'));
-        $menu->addChild('Laboratorium 1');
-        $menu['Laboratorium 1']->addChild('Pomiary', array('route' => 'lab1_pomiar_index'));
-        $menu['Laboratorium 1']->addChild('Wyniki', array('route' => 'lab1_wynik_index'));
+        $menu->addChild('Labs', array(
+            'uri' => '#',
+            'label' => ' <i class="fa fa-sitemap fa-fw"></i> Laboratorium<span class="fa arrow"></span>',
+            'extras' => array('safe_label' => true),
+        ));
+        $menu['Labs']->setChildrenAttribute('class', 'nav nav-second-level');
+        $menu['Labs']->addChild('Lab1', array(
+            'uri' => '#',
+            'label' => '1. Próba statyczna rozciągania<span class="fa arrow"></span>',
+            'extras' => array('safe_label' => true),
+        ));
+        $menu['Labs']['Lab1']->setChildrenAttribute('class', 'nav nav-third-level');
+        $menu['Labs']['Lab1']->addChild('Pomiary', array(
+            'route' => 'lab1_pomiar_index',
+            'label' => '<i class="fa fa-edit fa-fw"></i> Pomiary',
+            'extras' => array('safe_label' => true),
+        ));
+        $menu['Labs']['Lab1']->addChild('Wyniki', array(
+            'route' => 'lab1_wynik_index',
+            'label' => '<i class="fa fa-bar-chart-o fa-fw"></i> Wyniki',
+            'extras' => array('safe_label' => true),
+        ));
+        $menu['Labs']->addChild('Lab2', array(
+            'uri' => '#',
+            'label' => '2. Próba statyczna ściskania<span class="fa arrow"></span>',
+            'extras' => array('safe_label' => true),
+        ));
+        $menu['Labs']['Lab2']->setChildrenAttribute('class', 'nav nav-third-level');
+        $menu['Labs']['Lab2']->addChild('Pomiary', array(
+            'route' => 'lab2_pomiar_index',
+            'label' => '<i class="fa fa-edit fa-fw"></i> Pomiary',
+            'extras' => array('safe_label' => true),
+        ));
+        $menu['Labs']['Lab2']->addChild('Wyniki', array(
+            'route' => 'lab2_wynik_index',
+            'label' => '<i class="fa fa-bar-chart-o fa-fw"></i> Wyniki',
+            'extras' => array('safe_label' => true),
+        ));
 
+        /*$securityContext = $this->get('security.authorization_checker');
+        if($securityContext->isGranted('ROLE_ADMIN'))*/
+
+            $menu->addChild(
+                'Dashboard',
+                array(
+                    'uri' => '#',
+                    'label' => '<i class="fa fa-dashboard fa-fw"></i> Kokpit',
+                    'extras' => array('safe_label' => true),
+                )
+            );
+            $menu->addChild(
+                'Settings',
+                array(
+                    'uri' => '#',
+                    'label' => '<i class="fa fa-wrench fa-fw"></i> Ustawienia',
+                    'extras' => array('safe_label' => true),
+                )
+            );
 
         return $menu;
     }
