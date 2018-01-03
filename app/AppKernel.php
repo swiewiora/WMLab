b484b06a4585b0a6f9e4ef1a02b7cd1c5b6178cc
swiewior <sbswwr+bitbucket@gmail.com>
2018-01-03 21:39:44 +0100

    #32 APYDataGridBundle - installed KnpPaginatorBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle(),
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
