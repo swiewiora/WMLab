de4f0f8ac940bb7e6d9681fbab747d46fcce5e80
swiewior <sbswwr+bitbucket@gmail.com>
2018-01-14 22:28:25 +0100

    #26 add, show, edit projects - Edit Form

--- src/AppBundle/Form/ZwickEditType.php
+++ src/AppBundle/Form/ZwickEditType.php
@@ -17,60 +17,10 @@ class ZwickEditType extends ZwickType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('machine', ChoiceType::class, array(
-                'mapped' => false,
-                'choices'  => array(
-                    'Zwick' => 0,
-                ),
-                'attr' => array(
-                    'disabled' => null,
-                ),
-            ))
-            ->add('material', EntityType::class, array(
-                'class' => 'AppBundle:Material',
-                'choices' => $options['materials'],
-                'choice_label' => 'alloyName', ) )
-            ->add('shape', ChoiceType::class, array(
-                'choices'  => array(
-                    'Walec' => true,
-                    'Wiosełka' => false,
-                ),
-                'attr' => array(
-                    'disabled' => null,
-                ),
-            ))
-            ->add('type', ChoiceType::class, array(
-                'choices'  => array(
-                    'Rozciąganie' => true,
-                    'Ściskanie' => false,
-                ),
-            ))
-            ->add('d0', NumberType::class)
-            ->add('h0', NumberType::class)
-            ->add('t0', NumberType::class)
-            ->add('v0', NumberType::class)
-            ->add('ag', NumberType::class)
-            ->add('agt', NumberType::class)
-            ->add('at', NumberType::class)
-            ->add('e', NumberType::class)
-            ->add('r', NumberType::class)
-            ->add('fm', NumberType::class)
-            ->add('rm', NumberType::class)
-            ->add('rb', NumberType::class)
-            ;
-    }
-
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults(array(
-            'data_class' => Zwick::class,
-            'materials' => null,
-        ));
-    }
-
-    public function getBlockPrefix()
-    {
-        return 'app_bundle_zwick';
+      parent::buildForm($builder, $options);
+      $builder
+          ->remove('fileTRA')
+          ->remove('filePdf')
+      ;
     }
 }
